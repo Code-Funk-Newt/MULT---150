44c378c7697633f369c52c826c37a6336098db93
Code-Funk-Newt <100172196+Code-Funk-Newt@users.noreply.github.com>
2024-04-24 21:36:48 -0700

    Hour 11 Exercise

--- APretty_Hour 11/Assets/Scripts/PrefabGenerator.cs
+++ APretty_Hour 11/Assets/Scripts/PrefabGenerator.cs
@@ -4,16 +4,31 @@ public class PrefabGenerator : MonoBehaviour
 {
       public GameObject prefab;
 
+        
+      void Start(){
+            int x = 0;
+            
+
+            for(int i = 0; i < 10; i++){
+                x =- 2;
+
+                transform.position = transform.position + new Vector3(x,0,0);
+                
+                Instantiate(prefab, transform.position, transform.rotation);
+            }
+        }
+
       void Update()
       {
-          
+
           if (Input.GetKeyDown(KeyCode.B)){
                Instantiate(prefab);
           }
 
           if (Input.GetKeyDown(KeyCode.Space))
           {
-               Instantiate(prefab, transform.position, transform.rotation);
+
+               Instantiate(prefab, new Vector3(1,0,-5), transform.rotation);
           }
       }
 
