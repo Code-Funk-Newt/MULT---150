6838f75a06d10b8e46530970690a74912d209f44
Code-Funk-Newt <100172196+Code-Funk-Newt@users.noreply.github.com>
2024-05-03 17:29:46 -0700

    Hour_14and16

---
+++ APretty_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,13 @@
+/*
+Particle Effects:
+
+- The Main Particle Module was edited to shorten the lifetime and start speed
+- The Emission Particle was edited to slow down the rate of the particles
+- The Shape Module was edited to give the particle a cone shape and a wider angle
+- Color over lifetime and speed were edited to give the particle a fun gradient 
+- Size over Lifetime was edited to make the particle shrink and grow while its cycle ran 
+- Texture Sheet Animation was edited to replace the particles with a custom sprite
+- Texture sheet animation was also used to make the sprite spin while running its cycle
+- The Tails module was used to create a spooky ribbon that complimented the sprial animation
+
+*/
