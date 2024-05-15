57fa0520b3fc43693b60312d072ae829b0136ebc
Code-Funk-Newt <100172196+Code-Funk-Newt@users.noreply.github.com>
2024-05-14 20:08:23 -0700

    Hour 20 - upload

---
+++ APretty_Hour 20/Assets/GameCritic.cs
@@ -0,0 +1,34 @@
+/*
+Game Critic for Hour 20:
+
+Q1)Is the game too easy or hard ?
+A1) I think that the fact the character moves a bit slow makes things difficult. I feel 
+like I am dragged down while controlling them. I also think the camera location is a 
+bit awkward and makes it hard to anticipate upcoming obstacles. I think think thing 
+the game is hard, I think think its a little janky. 
+
+Q2) What 2 changes would you do to make it easier or harder?
+A2) I think making the character move faster and the adjusting the camera angle would make 
+things a whole lot easier. I would also like to change the time counting method. I would 
+like to collect more time so then it builds up. Maybe have vairous powerups with different
+times. It would make things eaiser, instead of the time method thats currently in use now. 
+
+Q3)List 2 things that would give it a "wow" factor?
+A3)I find that the theme and style of the game is sorta of boring, typical from a video game.
+If Im answering on a personal bais, I love to see a aquatic theme implimented. Maybe the 
+character could be a jetskiier and the obstacles could be puffer fish. 
+
+Q3)What 2 parts of the game are fun? 
+A3) I think the randomization and rate of spawn of the obstacles is fun. It does not treat 
+the player dumb. I think that you could Phase Out is a fun option, if the player wants to get 
+a bit tricky with their play style. 
+
+Q4)What 2 parts of the game are tedious?
+A4) I think the speed of the player is very tedious. This could be worked on. I also dislike the 
+camera angle. I think the timer is tedious as well. Why cant I collect more time?  
+
+
+
+
+*/
+
